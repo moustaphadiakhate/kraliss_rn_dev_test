118eb1b8e8118f389bcfbc82f4cf00cd3eb67c47
moustaphadiakhate <moustaphadiakhate1212@gmail.com>
2021-05-26 08:09:03 +0000

    [02] pdf screen

--- src/screens/PdfSreen.js
+++ src/screens/PdfSreen.js
@@ -1,8 +1,47 @@
-import React from "react";
-import { View } from "react-native";
+import React from 'react';
+import {StyleSheet, Dimensions, View} from 'react-native';
 
+import Pdf from 'react-native-pdf';
 const PdfScreen = () => {
-  return <View></View>;
+  const source = {
+    uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
+    cache: true,
+  };
+
+  return (
+    <View style={styles.container}>
+      <Pdf
+        source={source}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`current page: ${page}`);
+        }}
+        onError={error => {
+          console.log(error);
+        }}
+        onPressLink={uri => {
+          console.log(`Link presse: ${uri}`);
+        }}
+        style={styles.pdf}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 25,
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default PdfScreen;
