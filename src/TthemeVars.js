d118eabf47904cac0afa3013b661a2bb75532cc5
moustaphadiakhate <moustaphadiakhate1212@gmail.com>
2021-05-26 09:48:39 +0000

    minor: create a style variables files

---
+++ src/TthemeVars.js
@@ -0,0 +1,10 @@
+export default {
+  Colors: {
+    white: 'rgba(255, 255, 255, 1)',
+    green: 'rgba(102, 218, 178, 1)',
+    orange: 'rgba(246, 191, 76, 1)',
+  },
+  Fonts: {
+    general: 'Roboto',
+  },
+};
