22d0e1430059b2d3e33f1cad71d4c52c8507727c
moustaphadiakhate <moustaphadiakhate1212@gmail.com>
2021-05-27 11:10:23 +0000

    [06] create screens

---
+++ src/screens/CreateUserScreen.js
@@ -0,0 +1,129 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Image,
+} from 'react-native';
+import {connect} from 'react-redux';
+import {addUser} from '../store/reducer';
+import Theme from '../ThemeVars';
+import Modal from '../components/common/Modal';
+
+class CreateUserScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nom: '',
+      prenom: '',
+      triedCreate: false,
+    };
+  }
+
+  onClickListener = () => {
+    this.props.addUser({nom: this.state.nom, prenom: this.state.prenom});
+    this.setState({triedCreate: true, nom: '', prenom: ''});
+  };
+
+  errorHandler = () => {
+    return this.props.newUser.triedCreate && this.state.triedCreate;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Modal
+          modalVisible={this.errorHandler()}
+          message={
+            this.props.newUser.success
+              ? this.props.newUser.data
+              : this.props.newUser.error
+          }
+          success={this.props.newUser.success}
+          onRequestClose={() => this.setState({triedCreate: false})}
+        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Nom"
+            keyboardType="default"
+            underlineColorAndroid="transparent"
+            onChangeText={nom => this.setState({nom})}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Prenom"
+            underlineColorAndroid="transparent"
+            onChangeText={prenom => this.setState({prenom})}
+          />
+        </View>
+
+        <TouchableHighlight
+          style={[styles.buttonContainer, styles.createButton]}
+          onPress={() => this.onClickListener()}
+          disabled={this.props.newUser.loading}>
+          <Text style={styles.createText}>Ajouter</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  let newUser = {...state.newUser};
+  console.log('mapStateToProps state: newUser ' + JSON.stringify(newUser));
+  return {
+    newUser: newUser,
+  };
+};
+
+const mapDispatchToProps = {
+  addUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateUserScreen);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 5,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+  },
+  createButton: {
+    backgroundColor: Theme.Colors.green,
+  },
+  createText: {
+    color: 'white',
+  },
+});
