ec77e88d397f82d9d66792eb213dff654fc9cbfe
moustaphadiakhate <moustaphadiakhate1212@gmail.com>
2021-05-27 09:20:59 +0000

    minor: clean up code
    on utilisera pas de bottom tab ici mais seulement de notre menu.

--- src/navigation/home.js
+++ src/navigation/home.js
@@ -5,7 +5,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import DashboardScreen from '../screens/DashboardScreen';
 import UserListScreen from '../screens/UserListScreen';
-import UserDetailScreen from '../screens/UserDetailScreen';
+import DetailScreen from '../screens/DetailScreen';
 import CreateUserScreen from '../screens/CreateUserScreen';
 import ResourceScreen from '../screens/ResourceScreen';
 import ReduxDetailScreen from '../screens/ReduxDetailScreen';
@@ -13,54 +13,14 @@ import PdfSreen from '../screens/PdfSreen';
 
 const Stack = createStackNavigator();
 
-// const Tab = createBottomTabNavigator();
-
-// const BottomTabRouter = () => {
-//   return (
-//     <Tab.Navigator
-//       // tabBar={props => <CustomTabBar {...props} />}
-//       screenOptions={({route}) => ({
-//         tabBarIcon: ({focused, color, size}) => {
-//           let iconName;
-
-//           if (route.name === 'Home') {
-//             iconName = focused
-//               ? 'ios-information-circle'
-//               : 'ios-information-circle-outline';
-//           } else if (route.name === 'Settings') {
-//             iconName = focused ? 'ios-list-box' : 'ios-list';
-//           }
-
-//           // You can return any component that you like here!
-//           return <Ionicons name={iconName} size={size} color={color} />;
-//         },
-//       })}
-//       tabBarOptions={{
-//         activeTintColor: 'tomato',
-//         inactiveTintColor: 'gray',
-//       }}
-//       initialRouteName="Buzz">
-//       <Tab.Screen
-//         name="Profile"
-//         component={ProfileScreen}
-//         options={{
-//           iconName: 'profil',
-//           tabBarIcon: ({color, size}) => (
-//             <Ionicons name="home" color={color} size={size} />
-//           ),
-//         }}
-//       />
-//     </Tab.Navigator>
-//   );
-// };
-
 export default function HomeStack() {
   return (
     <Stack.Navigator initialRouteName="DashboardScreen" headerMode="none">
       <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
-      <Stack.Screen name="UserListScreen" component={UserListScreen} />
       <Stack.Screen name="CreateUserScreen" component={CreateUserScreen} />
       <Stack.Screen name="ResourceScreen" component={ResourceScreen} />
+      <Stack.Screen name="UserListScreen" component={UserListScreen} />
+      <Stack.Screen name="DetailScreen" component={DetailScreen} />
       <Stack.Screen name="ReduxDetailScreen" component={ReduxDetailScreen} />
       <Stack.Screen name="PdfSreen" component={PdfSreen} />
     </Stack.Navigator>
