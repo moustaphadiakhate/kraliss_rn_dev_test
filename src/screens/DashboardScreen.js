11894fffaea46fad894870a5c89ee4a71d01a789
moustaphadiakhate <moustaphadiakhate1212@gmail.com>
2021-05-27 11:05:28 +0000

    [06] create screens

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
+import Theme from '../ThemeVars';
+import Menu from '../components/common/Menu';
+
+const menu = [
+  {route: 'UserListScreen', description: 'User List'},
+  {route: 'CreateUserScreen', description: 'Create User'},
+  {route: 'ReduxDetailScreen', description: 'Redux Details'},
+  {route: 'ResourceScreen', description: 'Resources'},
+  {route: 'PdfSreen', description: 'PDF'},
+];
 
 const DashboardScreen = ({navigation}) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('UserListScreen')}
-        style={styles.circle}>
-        <Text style={styles.text}>UserListScreen</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => navigation.navigate('ReduxDetailScreen')}
-        style={styles.circle}>
-        <Text style={styles.text}>ReduxDetailScreen</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Menu style={styles.menu} items={menu} />
     </View>
   );
 };
@@ -27,20 +22,13 @@ const DashboardScreen = ({navigation}) => {
 export default DashboardScreen;
 
 const styles = {
-  circle: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
+  container: {
+    flex: 1,
     justifyContent: 'center',
-    backgroundColor: 'rgba(102, 218, 178, 1)',
-    borderWidth: 2,
-    marginBottom: 5,
-    borderColor: 'rgba(246, 191, 76, 1)',
   },
-  text: {
-    fontSize: 15,
-    fontWeight: '700',
-    color: 'white',
-    textAlign: 'center',
+  menu: {
+    alignContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 };
