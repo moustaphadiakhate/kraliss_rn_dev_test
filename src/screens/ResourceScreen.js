25c7c4859435536dd1f81db374581f264477691b
moustaphadiakhate <moustaphadiakhate1212@gmail.com>
2021-05-27 11:09:08 +0000

    [06] create screens

---
+++ src/screens/ResourceScreen.js
@@ -0,0 +1,113 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Alert,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import {connect} from 'react-redux';
+import {getResourceList} from '../store/reducer';
+import Theme from '../ThemeVars';
+
+class ResourceScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.props.getResourceList();
+  }
+
+  onClickListener = item => {
+    this.props.navigation.navigate('DetailScreen', {
+      type: 'resource',
+      data: item,
+    });
+  };
+
+  render() {
+    const {data} = this.props.data;
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          style={styles.itemList}
+          data={data || []}
+          keyExtractor={item => {
+            return item.id;
+          }}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity
+                onPress={() => this.onClickListener(item)}
+                style={styles.itemContainer}>
+                <View
+                  style={[styles.colorZone, {backgroundColor: item.color}]}
+                />
+
+                <Text
+                  style={
+                    styles.name
+                  }>{`${item.name}  ${item.year}  ${item.pantone_value}`}</Text>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  let resources = {...state.resources};
+  console.log('mapStateToProps state: resources' + JSON.stringify(resources));
+  return {
+    data: resources.data,
+    error: resources.error,
+    loading: resources.loading,
+  };
+};
+
+const mapDispatchToProps = {
+  getResourceList,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ResourceScreen);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB',
+  },
+  itemList: {
+    marginTop: 20,
+    padding: 10,
+  },
+  itemContainer: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 5,
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
+  colorZone: {
+    width: 45,
+    height: 45,
+    borderRadius: 20,
+    marginLeft: 20,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginLeft: 10,
+    alignSelf: 'center',
+  },
+});
--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -1,8 +1,125 @@
-import React from "react";
-import { View } from "react-native";
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert,
+  ScrollView,
+  FlatList,
+} from 'react-native';
+import {connect} from 'react-redux';
+import {getUsersList} from '../store/reducer';
+import Theme from '../ThemeVars';
 
-const UserListScreen = () => {
-  return <View></View>;
+class UserListScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.props.getUsersList();
+  }
+
+  onClickListener = item => {
+    this.props.navigation.navigate('DetailScreen', {type: 'user', data: item});
+  };
+
+  renderItem = ({item}) => {
+    return (
+      <TouchableOpacity onPress={() => this.onClickListener(item)}>
+        <View style={styles.row}>
+          <Image source={{uri: item.avatar}} style={styles.pic} />
+          <View>
+            <View style={styles.nameContainer}>
+              <Text
+                style={styles.nameTxt}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {item.first_name + ' ' + item.last_name}
+              </Text>
+            </View>
+            <View style={styles.msgContainer}>
+              <Text style={styles.msgTxt}>{item.email}</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    const {data} = this.props.data;
+
+    return (
+      <View style={{flex: 1}}>
+        <FlatList
+          extraData={this.state}
+          data={data || []}
+          keyExtractor={item => {
+            return item.id;
+          }}
+          renderItem={this.renderItem}
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  let users = {...state.users};
+  console.log('mapStateToProps state: userlist' + JSON.stringify(users));
+  return {
+    data: users.data,
+    error: users.error,
+    loading: users.loading,
+  };
 };
 
-export default UserListScreen;
+const mapDispatchToProps = {
+  getUsersList,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserListScreen);
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#DCDCDC',
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  pic: {
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 280,
+  },
+  nameTxt: {
+    marginLeft: 15,
+    fontWeight: '600',
+    color: '#222',
+    fontSize: 18,
+    width: 170,
+  },
+  msgContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  msgTxt: {
+    fontWeight: '400',
+    color: '#008B8B',
+    fontSize: 12,
+    marginLeft: 15,
+  },
+});
